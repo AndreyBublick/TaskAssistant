0c881dcd5bf2b3abe0262cf51a15e0e684ac0741
Andrew <dracon.24041230@mail.ru>
2025-01-09 15:14:35 +0300

    fix

--- src/store/selectors/tasks-selectors.ts
+++ src/store/selectors/tasks-selectors.ts
@@ -1,5 +1,5 @@
 import {RootStateType} from "../store";
-import {TodoListType} from "../../App";
+
 import {TaskItemType} from "../task-reducer/task-reducer";
 
 export const selectorGetTasks = (state:RootStateType):TaskItemType => state.tasks;
