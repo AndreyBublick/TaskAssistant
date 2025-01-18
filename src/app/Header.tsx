afc3f3bb36e7d3f35a40a14bb46c1caf27c88dae
Andrew <dracon.24041230@mail.ru>
2025-01-18 15:01:17 +0300

    global_Refactor_0.5

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,29 +1,25 @@
-import React, {memo, useCallback, useMemo, useState} from 'react';
-import {AppBar, Box, Button, IconButton, Switch, Toolbar, Typography} from "@mui/material";
+import React, {memo, useCallback, useState} from 'react';
+import {AppBar, Box, Button, IconButton, Switch, Toolbar} from "@mui/material";
 import {Menu} from "@mui/icons-material";
-import styled from "styled-components";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {useAppSelector} from "../hooks/Hooks";
 import {getModeTheme} from "./app-selectors";
 import {changeThemeModeAC} from "./app-reducer";
-import {getTheme} from "./theme/Theme";
+import {styled} from '@mui/material/styles';
 
 export const Header = memo(() => {
 
-    const [value,setValue] = useState("1");
+    const [value, setValue] = useState("1");
     const themeMode = useAppSelector(getModeTheme);
     const dispatch = useDispatch();
 
-    const changeThemeMode = useCallback(()=>{
-        dispatch(changeThemeModeAC({themeMode: themeMode==='light'?'dark':'light'}));
-    },[dispatch,themeMode]);
+    const changeThemeMode = useCallback(() => {
+        dispatch(changeThemeModeAC({themeMode: themeMode === 'light' ? 'dark' : 'light'}));
+    }, [dispatch, themeMode]);
 
-    const theme = useMemo(()=>{
-       return  getTheme(themeMode);
-    },[themeMode]) ;
 
-    return <AppBarStyled position="static" color="secondary">
-        <Toolbar>
+    return <AppBarStyled   position="static">
+        <ToolbarStyled>
             <IconButton
                 size="large"
                 edge="start"
@@ -33,22 +29,64 @@ export const Header = memo(() => {
             >
                 <Menu/>
             </IconButton>
-            <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                News
-            </Typography>
 
-            <Box sx={{display:'flex',gap:'15px'}}>
-                <Button sx={{boxShadow: `4px 4px 2px 0px ${theme.palette.primary.dark}`}} onClick={()=>setValue('1')} variant={'contained'} >Login</Button>
-                <Button sx={{boxShadow: `4px 4px 2px 0px ${theme.palette.primary.dark}`}}  onClick={()=>setValue('2')} variant={'contained'}>Two</Button>
-                <Button sx={{boxShadow: `4px 4px 2px 0px ${theme.palette.primary.dark}`}}  onClick={()=>setValue('3')} variant={'contained'}>Three</Button>
-                <Switch onChange={changeThemeMode} />
+
+            <Box sx={{display: 'flex', gap: '15px'}}>
+
+                 <ButtonStyled defaultValue={value} onClick={() => setValue('1')} variant={'contained'}>Login</ButtonStyled>
+                <ButtonStyled defaultValue={value} onClick={() => setValue('2')} variant={'contained'}>Two</ButtonStyled>
+                <ButtonStyled defaultValue={value} onClick={() => setValue('3')} variant={'contained'}>Three</ButtonStyled>
+                <Switch onChange={changeThemeMode}/>
             </Box>
 
 
-        </Toolbar>
+        </ToolbarStyled>
     </AppBarStyled>
 });
 
-const AppBarStyled = styled(AppBar)`
-    margin-bottom: 30px;
-`;
+
+const AppBarStyled = styled(AppBar)(({theme}) => ({
+    marginBottom:'30px',
+    backgroundColor: theme.palette.primary.light,
+    ...(theme.palette.mode === 'dark' && {
+        backgroundColor: '#333',
+    }),
+
+}));
+
+const ToolbarStyled = styled(Toolbar)({
+    display: 'flex',
+    justifyContent: 'space-between',
+
+})
+
+
+/*const ButtonStyled = styled(Button)({
+    boxShadow: `4px 4px 2px 0px ${theme.palette.primary.dark}`,
+    '&:hover': {backgroundColor: 'white'},
+});*/
+/*const ButtonStyled = styled(Button)(({ theme }) => ({
+    boxShadow: `4px 4px 2px 0px ${theme.palette.primary.dark}`,
+    '&:hover': {backgroundColor: 'white'},
+}));*/
+/*
+const ButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
+    boxShadow: `4px 4px 2px 0px ${theme.palette.primary.dark}`,
+    '&:hover': {backgroundColor: 'white'},
+}));
+*/
+
+
+ const ButtonStyled = styled(Button)(({theme}) => ({
+
+    boxShadow: `4px 4px 0px -0px ${theme.palette.primary.dark}`,
+    '&:hover': {
+       /* backgroundColor: 'white',*/
+        boxShadow: `4px 4px 2px 0px ${theme.palette.primary.light}`,
+    },
+    minWidth:100,
+     fontWeight:600,
+     border: `2px solid ${theme.palette.primary.dark}`,
+
+}));
+
--- src/app/theme/Theme.ts
+++ src/app/theme/Theme.ts
@@ -5,18 +5,26 @@ export type ThemeModeType = 'light'| 'dark';
 export const getTheme = (themeMode:ThemeModeType)=>{
 
 
-   return  createTheme({
+   return createTheme({
         palette: {
             primary: {
-                main: '#458965',
-                light:'#7AB898',
-                dark:'#2D5A43',
+                /*main: '#009688',
+                light:'#33ab9f',
+                dark:'#00695f',*/
+                main: '#009688',
+
+
             },
             secondary:{
-                main:'#F5F5F5',
+               /* main:'#00e5ff',
+                dark:'#00a0b2',
+                light:'#33eaff',*/
+                main: '#f5f5f5',
             },
             mode: themeMode,
+
         },
+
     });
 
 };
