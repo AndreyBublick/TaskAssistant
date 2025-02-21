c7f74131417f3c180c148be4e25b9b04c0125004
Andrew <dracon.24041230@mail.ru>
2025-02-21 21:55:25 +0300

    mini refactoring

--- src/common/utils/utils.ts
+++ src/common/utils/utils.ts
@@ -1,12 +1,12 @@
-import { changeAppStatus, setAppError } from "app/app-reducer";
-import type { GetThunkAPI } from "@reduxjs/toolkit";
-import { AppStatus } from "common/enums/enums";
+import { changeAppStatus, setAppError } from 'app/app-reducer';
+import type { GetThunkAPI } from '@reduxjs/toolkit';
+import { AppStatus } from 'common/enums/enums';
 
-import type { ResponseType } from "common/types/types";
-import type { AxiosResponse } from "axios";
+import type { ResponseType } from 'common/types/types';
+import type { AxiosResponse } from 'axios';
 
 export const handleServerNetworkError = (data: { error: unknown; thunkAPI: GetThunkAPI<any> }) => {
-  const errorMessage = data.error instanceof Error ? data.error.message : "netWork error";
+  const errorMessage = data.error instanceof Error ? data.error.message : 'netWork error';
 
   data.thunkAPI.rejectWithValue(data.error);
   data.thunkAPI.dispatch(setAppError({ error: errorMessage }));
@@ -21,7 +21,12 @@ export const handleServerAppError = <T>(data: {
 
   const errorMessage = response.data.messages[0];
 
-  thunkAPI.dispatch(setAppError(errorMessage ? { error: errorMessage } : { error: "some Error" }));
+  thunkAPI.dispatch(setAppError(errorMessage ? { error: errorMessage } : { error: 'some Error' }));
 };
 
-export const updateArray = <T>(arr: T[], item: T) => (arr.includes(item) ? arr : [...arr, item]);
+/*export const updateArray = <T>(arr: T[], item: T) => (arr.includes(item) ? arr : [...arr, item]);*/
+
+export const getLastItem = <T>(arr: T[]): T => {
+  return arr[arr.length - 1];
+};
+const a = getLastItem(['', '2', '']);
